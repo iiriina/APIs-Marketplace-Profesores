43d08da1ea53924c607c12d7e2270d97eaecfdf2
iiriina <irinachacon@hotmail.com>
2023-09-21 00:01:56 -0300

    barra responsive 1er attempt

--- src/components/HomeApisComponent/index.jsx
+++ src/components/HomeApisComponent/index.jsx
@@ -2,144 +2,23 @@ import React from "react";
 
 export const HomeApisComponent = () => {
     return (
-        <div className="slide">
-        <div className="overlap-wrapper">
-          <div className="overlap">
-            <header className="header">
-              <div className="rectangle" />
-              <div className="menu">
-                <div className="text-wrapper">Home</div>
-                <div className="text-wrapper">Payment</div>
-                <div className="text-wrapper">Features</div>
-              </div>
-              <div className="header-button">
-                <button className="contact-button">
-                  <div className="overlap-group">
-                    <div className="contact-us">Sign Up</div>
-                  </div>
-                </button>
-                <div className="div">Login</div>
-              </div>
-              <img className="logo" alt="Logo" src="https://c.animaapp.com/u1faXtVK/img/logo.svg" />
-            </header>
-            <div className="frame">
-              <div className="frame-2">
-                <div className="search-bar">
-                  <div className="overlap-group-2">
-                    <div className="placeholder">matemática, física, ...</div>
-                    <div className="lupa">
-                      <img
-                        className="cono-lupa"
-                        alt="Cono lupa"
-                        src="https://c.animaapp.com/u1faXtVK/img/-cono-lupa@2x.png"
-                      />
-                    </div>
-                  </div>
-                </div>
-                <div className="overlap-group-wrapper">
-                  <div className="overlap-2">
-                    <div className="filtro-categora">
-                      <div className="overlap-group-3">
-                        <img
-                          className="rectngulo-base"
-                          alt="Rectngulo base"
-                          src="https://c.animaapp.com/u1faXtVK/img/rect-ngulo-base-3.svg"
-                        />
-                        <div className="placeholder-2">Filtrar por Categoría</div>
-                      </div>
-                    </div>
-                    <div className="filtro-calificacin">
-                      <div className="overlap-group-3">
-                        <img
-                          className="img"
-                          alt="Rectngulo base"
-                          src="https://c.animaapp.com/u1faXtVK/img/rect-ngulo-base-2.svg"
-                        />
-                        <div className="placeholder-2">Filtrar por Calificación</div>
-                      </div>
-                    </div>
-                    <div className="filtro-clase">
-                      <div className="overlap-group-3">
-                        <img
-                          className="rectngulo-base-2"
-                          alt="Rectngulo base"
-                          src="https://c.animaapp.com/u1faXtVK/img/rect-ngulo-base-1.svg"
-                        />
-                        <div className="placeholder-2">Filtrar por Clase</div>
-                      </div>
-                    </div>
-                    <div className="div-wrapper">
-                      <div className="overlap-3">
-                        <img
-                          className="rectngulo-base-3"
-                          alt="Rectngulo base"
-                          src="https://c.animaapp.com/u1faXtVK/img/rect-ngulo-base.svg"
-                        />
-                        <div className="placeholder-3">Filtrar por Frecuencia</div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="frame-3">
-                  <img
-                    className="la-educacin-que-se"
-                    alt="La educacin que se"
-                    src="https://c.animaapp.com/u1faXtVK/img/la-educaci-n-que-se-adapta-a-vos.png"
-                  />
-                  <p className="p">
-                    Elegí entre miles de profesionales que te brindarán el conocimiento que estás buscando.
-                  </p>
-                  <div className="content">
-                    <button className="button">
-                      <div className="texto-comprar-wrapper">
-                        <div className="texto-comprar">Buscar</div>
-                      </div>
-                    </button>
-                  </div>
-                </div>
-              </div>
-              <div className="frame-4">
-                <div className="image">
-                  <div className="group">
-                    <img
-                      className="rectangle-2"
-                      alt="Rectangle"
-                      src="https://c.animaapp.com/u1faXtVK/img/rectangle-8.png"
-                    />
-                    <img
-                      className="rectangle-3"
-                      alt="Rectangle"
-                      src="https://c.animaapp.com/u1faXtVK/img/rectangle-10.png"
-                    />
-                  </div>
-                  <div className="group-2">
-                    <div className="overlap-group-4">
-                      <img
-                        className="dotted-shape"
-                        alt="Dotted shape"
-                        src="https://c.animaapp.com/u1faXtVK/img/dotted-shape@2x.png"
-                      />
-                      <img
-                        className="rectangle-4"
-                        alt="Rectangle"
-                        src="https://c.animaapp.com/u1faXtVK/img/rectangle-9.png"
-                      />
-                    </div>
-                  </div>
-                </div>
-                <p className="login">¿Quieres formar parte de nuestro equipo?</p>
-                <div className="contact-button-2">
-                  <div className="overlap-4">
-                    <div className="rectangle-5" />
-                    <div className="contact-us-2">Registrarse</div>
-                  </div>
-                </div>
-              </div>
+      <div className="mainContainer_PantallaPrincipal">
+        <div className="header">
+        <img className="logo" alt="Logo" src="https://c.animaapp.com/p8N1GDGe/img/logo.svg" />
+        <div className="menu">
+          <div className="text-wrapper">Inicio</div>
+          <div className="text-wrapper">Servicios</div>
+          <div className="text-wrapper">About Us</div>
+        </div>
+        <div className="header-button">
+          <div className="contact-button">
+            <div className="overlap-group">
+              <div className="contact-us">Log In</div>
             </div>
           </div>
         </div>
       </div>
-
+    </div>
     );
 };
 
